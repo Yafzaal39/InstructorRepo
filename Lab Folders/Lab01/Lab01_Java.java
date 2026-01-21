b8e7e5875c2bf79f1536022125a77d614ff3b768
Yafzaal39 <240136372@aston.ac.uk>
2026-01-21 15:38:08 +0000

    Update Lab01_Java.java

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,6 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
+  //Edit made by Yousuf Afzaal 240136372
 
 
   //Last line print('Good bye')
@@ -21,3 +22,4 @@ void main()
     testMethod1();
     testMethod2();
 }
+
